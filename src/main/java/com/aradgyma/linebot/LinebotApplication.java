40d38e68d6835cb2e5ba7ee099d9b8938e3b7294
tomo-chan <aradgyma@gmail.com>
2017-10-17 17:17:29 +0900

    Initial commit

---
+++ src/main/java/com/aradgyma/linebot/LinebotApplication.java
@@ -0,0 +1,12 @@
+package com.aradgyma.linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinebotApplication.class, args);
+	}
+}
