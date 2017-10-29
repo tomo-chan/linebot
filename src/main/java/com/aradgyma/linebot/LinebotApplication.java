0b276ae1670e8362a101c52ccb6ec89b9aad2e40
tomo-chan <aradgyma@gmail.com>
2017-10-29 11:58:43 +0900

    LINEBOTの処理を追加

--- src/main/java/com/aradgyma/linebot/LinebotApplication.java
+++ src/main/java/com/aradgyma/linebot/LinebotApplication.java
@@ -1,12 +1,30 @@
 package com.aradgyma.linebot;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@LineMessageHandler
 public class LinebotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinebotApplication.class, args);
 	}
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
 }
