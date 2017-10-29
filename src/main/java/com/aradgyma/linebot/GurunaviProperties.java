7d9b15a42ccd9065fd6efba0cfac0bc56fb441d1
tomo-chan <aradgyma@gmail.com>
2017-10-29 22:13:19 +0900

    アクセスキー取得処理を変更

--- src/main/java/com/aradgyma/linebot/GurunaviProperties.java
+++ src/main/java/com/aradgyma/linebot/GurunaviProperties.java
@@ -3,11 +3,13 @@ package com.aradgyma.linebot;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@Component
 @Validated
 @Configuration
 @ConfigurationProperties(prefix = "gurunavi")
